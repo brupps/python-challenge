91b15e4e84f553a7b5eae4d1656c9800a7c61162
brupps <barbrupps@gmail.com>
2023-11-12 16:56:26 -0600

    Working through getting the total

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,7 +8,11 @@
 import csv
 import os
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+#Lists to store data
+dates = []
+profits_losses = []
 
 with open(csvpath) as csvfile:
     
@@ -18,5 +22,26 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
- 
 
+
+    months = sum(1 for row in csvreader)
+    print("Financial Analysis")
+    print(" ")
+    print("-----------------------------")
+    print(" ")
+    print(f"Months: {int(months)}")
+
+#for row in csvreader:
+    #profits_losses.append(row[1])
+    #sum_prof_loss= 0
+    #sum_prof_loss = sum(int(number)for number in profits_losses)
+    #print(f"The total is: {sum_prof_loss}")
+
+#Sum as funciton
+def csv_total(st):
+    total = 0
+    with open(st)as a:
+        for line in csvreader:
+            total=total+int(line[1])
+            return total
+        print("The total is: {csv_total}") 
