3765613c67319665525aa81c12979a5a7a613330
brupps <barbrupps@gmail.com>
2023-11-13 16:37:43 -0600

    Finished all the calculations; code running as expected. Still need to output to a text file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,42 +11,82 @@ import os
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 #Lists to store data
-dates = []
+months = []
 profits_losses = []
+changes = []
 
-with open(csvpath) as csvfile:
+#Set Up Output print area
+print("Financial Analysis")
+print(" ")
+print("-----------------------------")
+print(" ")
+
+with open(csvpath, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    #print(csv_reader)
+    csv_header = next(csv_reader)
     
-    csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
-
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-
-
-    months = sum(1 for row in csvreader)
-    print("Financial Analysis")
-    print(" ")
-    print("-----------------------------")
-    print(" ")
-    print(f"Total Months: {int(months)}")
-    print (" ")
+    for row in csv_reader:
+        months.append(row[0])
+        profits_losses.append(int(row[1]))
+    
+        if line_count == 0:
+            line_count += 1
+        else:
+            line_count += 1
+#Print Number of months
+print(f'Total Months: {line_count} ')
+print(" ")
 
 
-#Sum as funciton
+#Calculate the total in $
 def csv_total(st):
     total = 0
     with open(st) as a:
-        csvreader = csv.reader(a)
-        next(csvreader)
-        for line in csvreader:
+        csv_reader = csv.reader(a)
+        csv_header = next(csv_reader)
+
+        for line in csv_reader:
             total += int(line[1])
-
+            
     return total
-
 total = csv_total(csvpath)
 print(f"Total: ${total}")
 print(" ")
 
-#Calculate Average
+#Calculate the average change; first the change between each month then average those values
+def csv_total(st):
+    total = 0
+    with open(st) as a:
+        csv_reader = csv.reader(a)
+        csv_header = next(csv_reader)
 
+    
+for i in range (1, len(months)):
+        change = profits_losses[i]-profits_losses[i-1]
+        changes.append(change)
+        
+        average_change = sum(changes)/len(changes)
+
+print(f"Average Change is {average_change:.2f}")
+print(" ")
+
+#Calculate the greatest increase and decrease in profits as well as the corresponding dates and print them.
+greatest_increase = max(changes)
+greatest_increase_date = months[changes.index(greatest_increase) + 1]
+greatest_decrease = min(changes)
+greatest_decrease_date = months[changes.index(greatest_decrease)+ 1]
+
+print(f"Greatest Increase in Profits:  {greatest_increase_date} (${greatest_increase}) ")
+print(" ")
+print(f"Greatest Increase in Profits:  {greatest_decrease_date} (${greatest_decrease}) ")
+print(" ")
+
+
+
+
+
+
+
+    
